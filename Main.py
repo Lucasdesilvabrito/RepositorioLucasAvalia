bfe68eb79940b741062efc22bf89d9f5be16d82a
LUCAS SILVA DE BRITO <44275289889@info.local>
2026-03-17 19:05:33 -0300

    Adicao do calculador de media e preenchedor de notas

--- Main.py
+++ Main.py
@@ -11,3 +11,15 @@ dados = [
     ("Rodrigo Tadeu", [10,10,10,10,10])
 ]
 
+for i in range(len(dados)): #aqui o sistema ta lendo o nome e as notas de cada um
+    nome = dados[i][0]
+    notas = dados[i][1]
+
+    if len(notas) == 0:
+        print(f"A lista de notas de {nome} está vazia, adicione as notas")
+    
+    adicionar = notasAusentes(nome,notas) #adicionar notas que nao tem na lista
+
+    media = calcularMedia(notas) #media
+
+    print(f"O aluno {nome} fechou o semestre com nota média de: {media}")
